78fecf40154a3dda53b267c11149e146c789e90d
musa <musa@chb-technologies.ch>
2023-09-08 11:10:06 +0100

    changed default font to montserrat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Montserrat } from 'next/font/google';
 
+const montserrat = Montserrat({subsets: ["latin"]})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className={montserrat.className}> <Component {...pageProps} /></div>
 }
