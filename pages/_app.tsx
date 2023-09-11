db75b434f918b442e4e79a980eba1f2ed95917d3
musa <musa@chb-technologies.ch>
2023-09-11 17:15:46 +0100

    Aaron here is your expo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { AppProps } from 'next/app'
 import { Montserrat } from 'next/font/google';
 
-const montserrat = Montserrat({subsets: ["latin"]})
+const montserrat = Montserrat({ subsets: ["latin"] })
+const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={montserrat.className}> <Component {...pageProps} /></div>
+  return <QueryClientProvider client={queryClient}>
+    <div className={montserrat.className}> 
+    <Component {...pageProps} />
+    </div>
+    </QueryClientProvider>
 }
